e8f39afa08525b8880228c9e607fdacc801882e7
Joeri Pelgrims <pelgrimsjoeri@gmail.com>
2019-10-26 13:14:20 -0700

    Add project files.

---
+++ Esthetics Exercise/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using Parlor_Library;
+
+namespace Parlor_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool inMainMenu = true;
+            while (inMainMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Customer menu\n2. Appointments \n3. Product menu \n4. Exit program");
+                int choiceMM = Convert.ToInt32(Console.ReadLine());
+                switch (choiceMM)
+                {
+                    case 1:
+                        bool inCustomerMenu = true;
+                        while (inCustomerMenu)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("1. Show customer list\n2. Add new customer\n3. Remove a customer\n4. Exit menu");
+                            int choiceM1 = Convert.ToInt32(Console.ReadLine());
+                            switch (choiceM1)
+                            {
+                                case 1:
+                                    Customer.showCustomerList();
+                                    Console.WriteLine("Press Space to continue...");
+                                    Console.ReadKey();
+                                    break;
+                                case 2:
+                                    Customer.addCustomer();
+                                    break;
+                                case 3:
+                                    Customer.removeCustomer();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Exit program.");
+                                    inCustomerMenu = false;
+                                    break;
+                                default:
+                                    Console.WriteLine("Wrong input probably.");
+                                    break;
+                            }
+                        }
+                        break;
+                    case 2:
+                        bool inAppointmentsMenu = true;
+                        while (inAppointmentsMenu)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("1. Show appointments list\n2. Add new appointment\n3. Remove an appointment\n4. Exit menu");
+                            int choiceM2 = Convert.ToInt32(Console.ReadLine());
+                            switch (choiceM2)
+                            {
+                                case 1:
+                                    Appointments.showAppointmentsList();
+                                    Console.WriteLine("Press Space to continue...");
+                                    Console.ReadKey();
+                                    break;
+                                case 2:
+                                    Appointments.addAppointment();
+                                    break;
+                                case 3:
+                                    Appointments.removeAppointment();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Exit menu.");
+                                    inAppointmentsMenu = false;
+                                    break;
+                                default:
+                                    Console.WriteLine("Wrong input probably.");
+                                    break;
+                            }
+
+                        }
+                        break;
+                    case 3:
+                        bool inProductMenu = true;
+                        while (inProductMenu)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("1. Show product/price list\n2. Stock new product\n3. Remove a product\n4. Exit menu");
+                            int choiceM3 = Convert.ToInt32(Console.ReadLine());
+                            switch (choiceM3)
+                            {
+                                case 1:
+                                    Product.showProducts();
+                                    Console.WriteLine("Press Space to continue...");
+                                    Console.ReadKey();
+                                    break;
+                                case 2:
+                                    Product.stockProduct();
+                                    break;
+                                case 3:
+                                    Product.showProducts();
+                                    Product.sellProduct();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Exit menu.");
+                                    inProductMenu = false;
+                                    break;
+                                default:
+                                    Console.WriteLine("Wrong input probably.");
+                                    break;
+                            }
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Exit program.");
+                        inMainMenu = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
---
+++ Parlor Library/Class1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parlor_Library
+{
+    abstract public class Person
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string TelephoneNumber { get; set; }
+
+    }
+    public class Customer : Person
+    {
+        static public List<string> customers = new List<string>() { "Joeri Pelgrims", "Joey Pelgrims", "Monique Reynders", "Marcel Pelgrims", "Jose Pelgrims" };
+        public string Appointment { get; set; }
+        static public void addCustomer()
+        {
+            Console.Clear();
+            Console.WriteLine("Adding a new customer. Please write the full name:");
+            customers.Add(Console.ReadLine());
+        }
+        static public void showCustomerList()
+        {
+            Console.Clear();
+            Console.WriteLine("Customer list:");
+            int i = 1;
+            foreach (string customer in customers)
+            {
+                Console.Write($"[{i}] ");
+                Console.WriteLine(customer);
+                i++;
+            }
+        }
+        static public void removeCustomer()
+        {
+            Console.Clear();
+            Console.WriteLine("Removing a customer. Please select who to remove:");
+            Customer.showCustomerList();
+            customers.RemoveAt(int.Parse(Console.ReadLine()) - 1);
+        }
+        // Print a list of customers in order.
+
+        // TO DO:
+        // static public void editCustomer() { }
+        // Edit the email, phone number or name of a customer.
+        // static public void showCustomerDetails() { }
+        // Show the email, phone number and name of a customer.
+        // static public void removeCustomer() { }
+        // Remove a customer from the database.
+        /*public Customer()
+        {
+            name = "Unnamed Customer";
+            emailAddress = "someone@someplace.something";
+            telephoneNumber = "123-456";
+            appointments = "Unknown appointment";
+        }
+        public Customer(string customerName, string customerEmailAddress, string customerTelephoneNumber, string customerAppointments)
+        {
+            name = customerName;
+            emailAddress = customerEmailAddress;
+            telephoneNumber = customerTelephoneNumber;
+            appointments = customerAppointments;
+        }*/
+    }
+    public class Parlors
+    {
+        public string Location { get; set; }
+        public string Name { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string ServicesList { get; set; }
+        public string EmailAddress { get; set; }
+
+        static public void addParlor() { }
+        // Add a parlor to the database.
+        static public void removeParlor() { }
+        // Remove a parlor from the database.
+        static public void editParlor() { }
+        // Edit the data of the parlor.
+        static public void showParlors() { }
+        // Show a list of currently open parlors.
+
+        // Basic constructor. No parameters.
+        public Parlors()
+        {
+            Name = "Unnamed schools.";
+            Location = "Unnamed place.";
+            TelephoneNumber = "123-4567";
+            ServicesList = "Unlisted services.";
+            EmailAddress = "something@someone.someplace";
+        }
+        // Constructor overload 1. 4 parameters.
+        public Parlors(string parlorName, string parlorLocation, string parlorTelephoneNumber, string parlorServicesList, string parlorEmailAddress)
+        {
+            Name = parlorName;
+            Location = parlorLocation;
+            TelephoneNumber = parlorTelephoneNumber;
+            ServicesList = parlorServicesList;
+            EmailAddress = parlorEmailAddress;
+        }
+        // TODO: VALIDATION, TRIM, CASE
+
+        // Redefining the ToString() method so that its output is predictable. Do this for each class.
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append(", ");
+            sb.Append(Location);
+            sb.Append(", ");
+            sb.Append(TelephoneNumber);
+            sb.Append(", ");
+            sb.Append(EmailAddress);
+            return sb.ToString();
+        }
+    }
+    public class Appointments
+    {
+        public static Queue<string> appointments = new Queue<string>();
+
+        private string Date { get; set; }
+        private string Name { get; set; }
+        private string Purpose { get; set; }
+        private string List { get; set; }
+
+        public static void showAppointmentsList()
+        {
+            Console.Clear();
+            Console.WriteLine("Current appointments:");
+            foreach (string apt in appointments)
+            {
+                Console.WriteLine(apt);
+            }
+        }
+        public static void addAppointment()
+        {
+            Console.Clear();
+            Console.WriteLine("Create an appointment for:");
+            appointments.Enqueue(Console.ReadLine());
+        }
+        public static void removeAppointment()
+        {
+            Console.Clear();
+            Console.WriteLine("Complete appointment:");
+            appointments.Dequeue();
+        }
+        public static void editAppointment() { }
+        //public Appointments()
+        //{
+        //    name = "Unknown name";
+        //    date = "31/12/1999";
+        //    purpose = "Unknown purpose";
+        //    List = "Empty list";
+        //}
+        //public Appointments(string appointmentDate, string appointeeName, string appointmentPurpose, string appointmentList)
+        //{
+        //    name = appointeeName;
+        //    date = appointmentDate;
+        //    purpose = appointmentPurpose;
+        //    List = appointmentList;
+        //}
+    }
+    public class Services
+    {
+        private string name { get; set; }
+        private string cost { get; set; }
+        private string length { get; set; }
+        private string description { get; set; }
+        private string list { get; set; }
+
+        static public void showServicesList() { }
+        // Get a list of available services, their cost and length.
+        static public void addService() { }
+        // Add a service to the database.
+        static public void removeService() { }
+        // Remove a service from the database.
+        static public void showService() { }
+        // Show a specified service's details.
+        public Services()
+        {
+            name = "Unknown name";
+            cost = "Unknown cost";
+            length = "Unknown length";
+            description = "Unknown description";
+            list = "Unknown list";
+        }
+        public Services(string serviceName, string serviceCost, string serviceLength, string serviceDescription, string serviceList)
+        {
+            name = serviceName;
+            cost = serviceCost;
+            length = serviceLength;
+            description = serviceDescription;
+            list = serviceList;
+        }
+    }
+    public class Product
+    {
+        public static Dictionary<string, string> pricelist = new Dictionary<string, string>();
+
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+
+        static public void sellProduct()
+        {
+            Console.Clear();
+            Console.WriteLine("Removing product from inventory:");
+            pricelist.Remove(Console.ReadLine());
+        }
+
+        static public void stockProduct()
+        {
+            Console.Clear();
+            Console.WriteLine("Adding product to inventory:");
+            //pricelist.Add(Console.ReadLine(), Console.ReadLine());
+            pricelist[Console.ReadLine()] = Console.ReadLine();
+        }
+
+        static public void showProducts()
+        {
+            Console.Clear();
+            Console.WriteLine("Product list:");
+            foreach (KeyValuePair<string, string> product in pricelist)
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        static public void searchProducts() { }
+        // Search product catalog by name.
+        // TO DO:
+        /*
+        public Product()
+        {
+            list = "Unknown list";
+            description = "Unknown description.";
+            priceList = "Unknown pricelist.";
+        }
+        public Product(string productList, string productDescription, string productPriceList)
+        {
+            list = productPriceList;
+            description = productDescription;
+            priceList = productPriceList;
+        }
+        */
+    }
+}
+
